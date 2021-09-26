7b6a7a1cdfb6e082b9676e5efab59f943488a724
AlanMalikov777 <77801087+AlanMalikov777@users.noreply.github.com>
2021-09-26 19:00:29 +0600

    Crypocurrency top sorter
    Crypocurrency top sorter

---
+++ src/assignment1.py
@@ -0,0 +1,10 @@
+from pycoingecko import CoinGeckoAPI 
+def cryptoTop():   #function which will give 'n' numbers of cryptocurrency ordered by coingecko website top
+    cg = CoinGeckoAPI()
+    n=input()
+    num=int(n)
+    case1=cg.get_coins_markets(vs_currency= "usd", per_page=num)#var will take a list of dictionaries
+
+    for x in range(num):#loop will print a n number of cryptocurrency
+        print(case1[x].get("name"))
+
---
+++ test/test.py
@@ -0,0 +1,6 @@
+import sys
+print("Hello, dear guest! ")
+print("Please choose, how many cryptocurrency on top you want to see: ")
+sys.path.append("..")
+from src import assignment1
+assignment1.cryptoTop()
